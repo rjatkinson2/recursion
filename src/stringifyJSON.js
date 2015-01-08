ef4fcc53e7b4e1da4684313b044f52e10e0bd65f
Ryan Atkinson <rjatkinson2@gmail.com>
2015-01-08 15:24:29 -0800

    Added logic to handle undefined and function types.  Additionally, fixed the way the function handles objects by using obj[key] rather than just object and changing the requirement to and instead of or to ensure that only object values that aren't undefined AND aren't functions are being considered by the stringify.  Those that don't qualify are ignored.  At this point, the mocha test suite passes 100%, but I'm not sure escaped characters are handled properly as my functions output doesn't match JSON.stringify.  Will revisit after the rest of section 3 is complete!

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,6 +5,7 @@
 
 var stringifyJSON = function(obj) {
   //numbers
+  if(obj === undefined || typeof obj === 'function'){return undefined;}
   if(typeof obj === 'number'){
     var numToString = obj + "";
     return numToString;
@@ -32,13 +33,15 @@ var stringifyJSON = function(obj) {
     //look at each key value pair
     for(var key in obj){
       //handle the key stringification
-      keyValString = '';
-      keyValString += stringifyJSON(key);
-      keyValString += ":";
-      //handle the value stringification
-      keyValString += stringifyJSON(obj[key]);
-      //push each key value pair into an array
-      props.push(keyValString);
+      if(obj[key] !== undefined && typeof obj[key] !== 'function'){
+        keyValString = '';
+        keyValString += stringifyJSON(key);
+        keyValString += ":";
+        //handle the value stringification
+        keyValString += stringifyJSON(obj[key]);
+        //push each key value pair into an array
+        props.push(keyValString);
+      }
     }
     if(keyValString){
       return "{" + props.join(",") + "}";
@@ -58,64 +61,3 @@ var stringifyJSON = function(obj) {
 
   return obj;
 };
-
-
-
-  debug(JSON.stringify(9)===stringifyJSON(9));
-  debug(JSON.stringify(null)===stringifyJSON(null));
-  debug(typeof JSON.stringify(null));
-  debug(typeof stringifyJSON(null));
-  debug(JSON.stringify(true)===stringifyJSON(true));
-  debug(JSON.stringify(false)===stringifyJSON(false));
-  debug(JSON.stringify("Hello world")===stringifyJSON("Hello world"));
-  debug(JSON.stringify([])===stringifyJSON([]));
-  debug(JSON.stringify([8])===stringifyJSON([8]));
-  debug(JSON.stringify(["hi"])===stringifyJSON(["hi"]));
-  debug(JSON.stringify([8, "hi"])===stringifyJSON([8, "hi"]));
-  debug(JSON.stringify([1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999])===stringifyJSON([1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999]));
-  debug(JSON.stringify([8, [[],3,4]])===stringifyJSON([8, [[],3,4]]));
-  debug(JSON.stringify([[[["foo"]]]])===stringifyJSON([[[["foo"]]]]));
-  debug(JSON.stringify({})===stringifyJSON({}));
-  debug(JSON.stringify({})===stringifyJSON({"a": "apple"}));
-  debug(JSON.stringify({"a": "apple"})===stringifyJSON());
-  debug(JSON.stringify({"foo": true, "bar": false, "baz": null})===stringifyJSON({"foo": true, "bar": false, "baz": null}));
-  debug(JSON.stringify({"boolean, true": true, "boolean, false": false, "null": null })===stringifyJSON({"boolean, true": true, "boolean, false": false, "null": null }));
-  debug(JSON.stringify({"a":{"b":"c"}})===stringifyJSON({"a":{"b":"c"}}));
-  debug(JSON.stringify({"a":["b", "c"]})===stringifyJSON({"a":["b", "c"]}));
-  debug(JSON.stringify([{"a":"b"}, {"c":"d"}])===stringifyJSON([{"a":"b"}, {"c":"d"}]));
-  debug(JSON.stringify({"a":[],"c": {}, "b": true})===stringifyJSON({"a":[],"c": {}, "b": true}));
-
-
-/*
-debug('----numbers----');
-debug(JSON.stringify(56));                        // '{}'
-debug(stringifyJSON(56));                         // '{}'
-debug(typeof JSON.stringify(56));                        // '{}'
-debug(typeof stringifyJSON(56));                         // '{}'
-debug('----objects - blank----');
-debug(JSON.stringify({}));                        // '{}'
-debug(stringifyJSON({}));                         // '{}'
-debug(typeof JSON.stringify({}));                        // '{}'
-debug(typeof stringifyJSON({}));                         // '{}'
-debug('----booleans----');
-debug(JSON.stringify(true));                      // 'true'
-debug(stringifyJSON(true));                       // 'true'
-debug(typeof JSON.stringify(true));                      // 'true'
-debug(typeof stringifyJSON(true));                       // 'true'
-debug('----strings-----');
-debug(JSON.stringify("foo"));                     // '"foo"'
-debug(stringifyJSON("foo"));                      // '"foo"'
-debug(typeof JSON.stringify("foo"));                     // '"foo"'
-debug(typeof stringifyJSON("foo"));                      // '"foo"'
-debug('----arrays-----');
-debug(JSON.stringify([1, "false", false]));       // '[1,"false",false]'
-debug(stringifyJSON([1, "false", false]));        // '[1,"false",false]'
-debug(typeof JSON.stringify([1, "false", false]));       // '[1,"false",false]'
-debug(typeof stringifyJSON([1, "false", false]));        // '[1,"false",false]'
-debug('----objects-full----');
-debug(JSON.stringify({ x: 5, "y":9 }));                  // '{"x":5}'
-debug(stringifyJSON({ x: 5, "y":9 }));                   // '{"x":5}'
-debug(typeof JSON.stringify({ x: 5, "y":9 }));                  // '{"x":5}'
-debug(typeof stringifyJSON({ x: 5, "y":9 }));                   // '{"x":5}'
-
-*/
