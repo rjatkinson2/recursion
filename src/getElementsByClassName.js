6a2494eda362be108b0739434c2104eeb56e2a64
Ryan Atkinson <rjatkinson2@gmail.com>
2015-01-08 20:39:37 -0800

    Added some basic functionality that does the following: 1. Store all the child nodes in an array 2. loop through all of the child nodes 3. store each child nodes classList in an array 4. If the classList exists, and the classList has a length, loop through the classList. 5. If any of the classNames match the getElementByClassName argument, push that specific childNode to the matches array. 6. Finally, return the array of matches.
    In the test environment, this logic appears to obtain every node with the class name, so not sure why the tests aren't passing.  Need to investigate futher.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,12 +5,26 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
+  //Returns an array-like object of all child elements which have all of the given class names. When called on the document object, the complete document is searched, including the root node. You may also call getElementsByClassName() on any element; it will return only elements which are descendants of the specified root element with the given class names.
   //document.body - Returns the <body> or <frameset> node of the current document, or null if no such element exists.
-  //element.childNodes - Returns the child elements of the selected node in the form of Array.
+  //element.childNodes - Returns the child elements of the selected node in the form of Array.  Child nodes appear to be shown in an array listed by just there tag <p> is shown as p.  If there's nothing inside, it will just be a p, but if there's stuff inside it will be p <stuff> p in the array's order. The array is one-level but represents multi-level html.
   //element.classList - classList returns a token list of the class attribute of the element.
-  console.log(document.body);
-  console.log(document.body.childNodes);
-  console.log(document.body.classList); 
+  //console.log(document.body);
+  //console.log(document.body.childNodes);
+  //console.log(document.body.classList);
+  var childNodeArray = document.body.childNodes;
+  var matches = [];
+  for (var i = 0; i < childNodeArray.length; i++) {
+  	var cList = childNodeArray[i].classList;
+  	if(cList){
+  		if(cList.length){
+  			for (var j = 0; j < cList.length; j++) {
+  				if(cList[j]===className){matches.push(childNodeArray[i]);}
+  			}
+  		}
+  	}
+  }
+  return(matches);
 };
 
-getElementsByClassName('targetClassName');
+console.log(getElementsByClassName('targetClassName'));
