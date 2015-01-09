def11f352c8b77b4353e25d7c22a4d01f42b143d
Ryan Atkinson <rjatkinson2@gmail.com>
2015-01-08 20:09:03 -0800

    Added three suggested methods to consider and started taking notes on each method. Additionally, added getElementsTest.html with getElementsByClassName.js added for testing purposes, and logged the three methods to get a feel for what they produce while referencing some static html.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,12 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  
+  //document.body - Returns the <body> or <frameset> node of the current document, or null if no such element exists.
+  //element.childNodes - Returns the child elements of the selected node in the form of Array.
+  //element.classList - classList returns a token list of the class attribute of the element.
+  console.log(document.body);
+  console.log(document.body.childNodes);
+  console.log(document.body.classList); 
 };
+
+getElementsByClassName('targetClassName');
