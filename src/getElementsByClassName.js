57de5bd0b074a20b0ec309a874baa05a95ad82bb
Ryan Atkinson <rjatkinson2@gmail.com>
2015-01-12 10:59:50 -0800

    Hit a wall on previous attempts so going to try to reimplement from scratch.  Commented out the whole function body and will use bits and pieces along the way.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,7 +5,7 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  //instantiate a blank array to store matches
+  /*//instantiate a blank array to store matches
   var matches = [];
   //instantiate a blank array for recursive children matches
   var childrenMatches = [];
@@ -19,28 +19,37 @@ var getElementsByClassName = function(className){
   else{childNodeArray = arguments[1].childNodes;}
   //look at every child node
   for (var i = 0; i < childNodeArray.length; i++) {
-    //if the child node is a DIV, look through it recursively. need to add any other types that create layers.
-  	if(childNodeArray[i].nodeName === 'DIV'){
-  		//childrenMatches.push(getElementsByClassName(className, childNodeArray[i]));
-  	}
-    //store a list of classes associated with the node
-  	var classList = childNodeArray[i].classList;
-    //if the node has a classList, go onnnnnn
-  	if(classList){
-      //if the clasList has a length, or as classes basically, go onnn
-  		if(classList.length){
-        //loop through all of the classes
-  			for (var j = 0; j < classList.length; j++) {
-          //if any of the classes equal the listed class name, push the current node (childNode of the parent) to the matches array
-  				if(classList[j]===className){matches.push(childNodeArray[i]);}
-  			}
-  		}
-  	}
+    if(childNodeArray[i] instanceof HTMLElement){
+      console.log(childNodeArray[i].nodeName);
+      //if the child node is a DIV, look through it recursively. need to add any other types that create layers.
+    	if(childNodeArray[i].nodeName === 'DIV' || childNodeArray[i].nodeName === 'P'){
+    		childrenMatches.push(getElementsByClassName(className, childNodeArray[i])[0]);
+        //console.log(childrenMatches);
+    	}
+      //console.log(childrenMatches);
+      //store a list of classes associated with the node
+    	var classList = childNodeArray[i].classList;
+      //if the node has a classList, go onnnnnn
+    	if(classList){
+        //if the clasList has a length, or as classes basically, go onnn
+    		if(classList.length){
+          //loop through all of the classes
+    			for (var j = 0; j < classList.length; j++) {
+            //if any of the classes equal the listed class name, push the current node (childNode of the parent) to the matches array
+    				if(classList[j]===className){matches.push(childNodeArray[i]);}
+          }
+        }
+      }
+    }
   }
+  for (var i = 0; i < childrenMatches.length; i++) {
+    matches.push(childrenMatches[i]);
+  };
   return(matches);
+  //console.log(childrenMatches);*/
 };
 getElementsByClassName('targetClassName');
 console.log('---testing---')
-//console.log(document.getElementsByClassName('targetClassName') === getElementsByClassName('targetClassName'));
+console.log(document.getElementsByClassName('targetClassName') === getElementsByClassName('targetClassName'));
 console.log(document.getElementsByClassName('targetClassName'));
 console.log(getElementsByClassName('targetClassName'));
