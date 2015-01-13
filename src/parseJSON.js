86ed7803536c82b0434482b123db6e77bf111dde
Ryan Atkinson <rjatkinson2@gmail.com>
2015-01-13 12:15:54 -0800

    Added a number of test cases for parseJSON to better understand function requirements and test implementations, in addition to that provided by the spec. Addtionally, added some basic logic to parse first and last character of the string input to determine if we are dealing with an object or an array at the top-level.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,19 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  
+  var outer = json.slice(0,1) + json.slice(json.length-1,json.length);
+  //identify objects
+    if(outer === '{}'){debug('its an object');}
+  //identify arrays
+    if(outer === '[]'){debug('its an array');}
+  //everything else
 };
+parseJSON('[asdfasdfasdfasdfasdfsdf]');
+
+debug('==== test stuff ====')
+debug(JSON.parse('{}'));                 // {}
+debug(JSON.parse('true'));               // true
+debug(JSON.parse('"foo"'));              // "foo"
+debug(JSON.parse('[1, 5, "false"]'));    // [1, 5, "false"]
+debug(JSON.parse('null'));               // null
+debug(JSON.parse('{"1": 1, "2": 2}'));   //Object {1: 1, 2: 2}
