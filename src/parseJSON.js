db2e12cb2b804fa355e1259c21c3b19fb906c864
Ryan Atkinson <rjatkinson2@gmail.com>
2015-01-13 13:00:59 -0800

    Once an object is identified, parseJSON now counts the number of key-item pairs inside that object by counting the number of commas and adding one to the total if any commas are identified. Additionally, a skip counter has been implemented so that all commas inside quotations are ignored. This may not work for objects nested inside strings inside other objects, but outside cases will be tested later

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,13 +4,33 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   var outer = json.slice(0,1) + json.slice(json.length-1,json.length);
+  var inner = json.slice(1,json.length-1);
   //identify objects
-    if(outer === '{}'){debug('its an object');}
+    if(outer === '{}'){
+      debug('its an object');
+      var numInside = 0;
+      var skip = 0;
+      //count commas to determine number of objects inside the object.
+      for (var i = 0; i < inner.length; i++) {
+        if(inner.charAt(i) === "'"){
+          skip++;
+        }
+        if(inner.charAt(i) === ',' && skip % 2 === 0){
+          numInside++;
+        }
+      }
+      if(numInside > 0){
+        numInside++;
+      }
+      debug(numInside);
+    }
   //identify arrays
-    if(outer === '[]'){debug('its an array');}
+    if(outer === '[]'){
+      debug('its an array');
+    }
   //everything else
 };
-parseJSON('[asdfasdfasdfasdfasdfsdf]');
+parseJSON("{'a,bc':5,'def':14,'ghi':false, monkey:boy}");
 
 debug('==== test stuff ====')
 debug(JSON.parse('{}'));                 // {}
