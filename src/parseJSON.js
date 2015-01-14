8c8dce165d66e1c175639df5c8b174af5c27f485
Ryan Atkinson <rjatkinson2@gmail.com>
2015-01-13 20:28:33 -0800

    Added logic to handle arrays in addition to objects. Using recursion, the function then handles each piece of the array or object either again by array/object handling or via logic for booleans, numbers, and strings.  The logic for the former two has been completed as well.  Ultimately, the logic for string will be based on the premise that there's no other type left, since everything appears as a string unless you can verify it's something else. So, a few cases left to consider and then things should wrap up.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -5,38 +5,73 @@
 var parseJSON = function(json) {
   var outer = json.slice(0,1) + json.slice(json.length-1,json.length);
   var inner = json.slice(1,json.length-1);
-  //identify objects
-    if(outer === '{}'){
-      var pairs = [], lastComma = 0, numInside = 0, skip = 0;
-      //count commas to determine number of objects inside the object.
-      for (var i = 0; i < inner.length; i++) {
-        if(inner.charAt(i) === "'"){
-          skip++;
-        }
-        if(inner.charAt(i) === ',' && skip % 2 === 0){
-          pairs.push(inner.slice(lastComma,i));
-          lastComma = i+1;
-          numInside++;
-        }
+
+  //handle booleans
+  if(json === 'true' || json === 'false'){
+    console.log(typeof json);
+    return (json === 'true') ? true : false;
+  }
+  
+  //handle numbers
+  if(!isNaN(json)){
+    return Number(json);
+  }
+
+  //handle objects
+  if(outer === '{}'){
+    newObject = {};
+    var keyVals = parseDivider(inner,',');
+
+    //handle key-value pairs
+    var keyVal;
+    for (var i = 0; i < keyVals.length; i++) {
+      keyVal = parseDivider(keyVals[i],':');
+      newObject[keyVal[0]] = parseJSON(keyVal[1]);
+    }
+    console.log(newObject);
+  }
+
+  //handle arrays
+  if(outer === '[]'){
+    newArray = [];
+    var vals = parseDivider(inner,',');
+    for (var i = 0; i < vals.length; i++) {
+      newArray.push(parseJSON(vals[i]));
+    }
+    return newArray;
+  }
+
+  //everything else
+
+  //parseDivider is a utility function that searches for a divider and breaks the string up into an array of values demonstrated by the specified divider. The function makes sure the divider is skipped if it exists inside of a string.
+  function parseDivider(str,div){
+    var pairs = [], lastComma = 0, numInside = 0, skip = 0;
+    //count commas to determine number of objects inside the object.
+    for (var i = 0; i < str.length; i++) {
+      if(str.charAt(i) === "'"){
+        skip++;
       }
-      if(numInside > 0){
-        pairs.push(inner.slice(lastComma,inner.length));
+      if(str.charAt(i) === div && skip % 2 === 0){
+        pairs.push(str.slice(lastComma,i));
+        lastComma = i+1;
         numInside++;
       }
-      debug(pairs);
     }
-  //identify arrays
-    if(outer === '[]'){
-      debug('its an array');
+    if(numInside > 0){
+      pairs.push(str.slice(lastComma,str.length));
+      numInside++;
     }
-  //everything else
+    return pairs;
+  }
+  return json;
 };
-parseJSON("{'a,bc':5,'def':14,'ghi':false,monkey:boy}");
+console.log(parseJSON("[4,5,'boy']"));
+parseJSON("{'a,bc':5,'def':14,'ghi':true,monkey:boy}");
 
-debug('==== test stuff ====');
-debug(JSON.parse('{}'));                 // {}
-debug(JSON.parse('true'));               // true
-debug(JSON.parse('"foo"'));              // "foo"
-debug(JSON.parse('[1, 5, "false"]'));    // [1, 5, "false"]
-debug(JSON.parse('null'));               // null
-debug(JSON.parse('{"1": 1, "2": 2}'));   //Object {1: 1, 2: 2}
+console.log('==== test stuff ====');
+console.log(JSON.parse('{}'));                 // {}
+console.log(JSON.parse('true'));               // true
+console.log(JSON.parse('"foo"'));              // "foo"
+console.log(JSON.parse('[1, 5, "false"]'));    // [1, 5, "false"]
+console.log(JSON.parse('null'));               // null
+console.log(JSON.parse('{"1": 1, "2": 2}'));   //Object {1: 1, 2: 2}
