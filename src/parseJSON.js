9d7ddabcd72aafc1d490c54b01defa092fd09c05
Ryan Atkinson <rjatkinson2@gmail.com>
2015-01-14 17:21:27 -0800

    Added loop to set start of inner portion of json that should be analyzed. Basically, this loop ensures white space is skipped over at the start of any stringified JSON.  This produced desirable results in personally generated test case. However, still not passing the mocha spec.  Therefore, individual test cases from Mocha have been added for analysis.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,8 +3,22 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  var outer = json.slice(0,1) + json.slice(json.length-1,json.length);
-  var inner = json.slice(1,json.length-1);
+  var notSet = true;
+  for (var i = 0; i < json.length; i++) {
+    if(notSet && json.charAt(i) !== ' '){
+      var start = i;
+      notSet = false;
+    }
+  }
+
+  var inner, outer;
+  outer = json.slice(start,1) + json.slice(json.length-1,json.length);
+  inner = json.slice(start+1,json.length-1);
+
+  if(json.length === 1){
+    outer = '""';
+    inner = json;
+  }
 
   //handle null
   if(json === 'null'){
@@ -55,7 +69,7 @@ var parseJSON = function(json) {
     var pairs = [], lastComma = 0, numInside = 0, skip = 0;
     //count commas to determine number of objects inside the object.
     for (var i = 0; i < str.length; i++) {
-      if(str.charAt(i) === "'"){
+      if(str.charAt(i) === '"'){
         skip++;
       }
       if(str.charAt(i) === div && skip % 2 === 0){
@@ -73,11 +87,52 @@ var parseJSON = function(json) {
   return json;
 };
 
-console.log(parseJSON("[4,5,'girl',false]"));
-parseJSON("{'a,bc':5,'def':14,'ghi':true,monkey:boy}");
+console.log(JSON.parse('[]'));
+console.log(parseJSON('[]'));
+console.log(JSON.parse('{"foo": ""}'));
+console.log(parseJSON('{"foo": ""}'));
+console.log(JSON.parse('{}'));
+console.log(parseJSON('{}'));
+console.log(JSON.parse('{"foo": "bar"}'));
+console.log(parseJSON('{"foo": "bar"}'));
+console.log(JSON.parse('["one", "two"]'));
+console.log(parseJSON('["one", "two"]'));
+console.log(JSON.parse('{"a": "b", "c": "d"}'));
+console.log(parseJSON('{"a": "b", "c": "d"}'));
+console.log(JSON.parse('[null,false,true]'));
+console.log(parseJSON('[null,false,true]'));
+console.log(JSON.parse('{"foo": true, "bar": false, "baz": null}'));
+console.log(parseJSON('{"foo": true, "bar": false, "baz": null}'));
+console.log(JSON.parse('[1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999]'));
+console.log(parseJSON('[1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999]'));
+console.log(JSON.parse('{"boolean, true": true, "boolean, false": false, "null": null }'));
+console.log(parseJSON('{"boolean, true": true, "boolean, false": false, "null": null }'));
+// basic nestin
+// basic nestin
+console.log(JSON.parse('{"a":{"b":"c"}}'));
+console.log(parseJSON('{"a":{"b":"c"}}'));
+console.log(JSON.parse('{"a":["b", "c"]}'));
+console.log(parseJSON('{"a":["b", "c"]}'));
+console.log(JSON.parse('[{"a":"b"}, {"c":"d"}]'));
+console.log(parseJSON('[{"a":"b"}, {"c":"d"}]'));
+console.log(JSON.parse('{"a":[],"c": {}, "b": true}'));
+console.log(parseJSON('{"a":[],"c": {}, "b": true}'));
+console.log(JSON.parse('[[[["foo"]]]]'));
+console.log(parseJSON('[[[["foo"]]]]'));
+// escapin
+// escapin
+console.log(JSON.parse('["\\\\\\"\\"a\\""]'));
+console.log(parseJSON('["\\\\\\"\\"a\\""]'));
+console.log(JSON.parse('["and you can\'t escape thi\s"]'));
+console.log(parseJSON('["and you can\'t escape thi\s"]'));
+
+console.log('==== my stuff ====');
+console.log(JSON.parse('[4,5,"girl",false]'));
+console.log(parseJSON('[4,5,"girl",false]'));
+console.log(JSON.parse('{"a,bc":5,"de:f":14,"ghi":true,"monkey": "boy"}'));
+console.log(parseJSON('{"a,bc":5,"de:f":14,"ghi":true,"monkey": "boy"}'));
 
 console.log('==== test stuff ====');
-console.log(JSON.parse('[4,5,"girl",false]'));
 console.log(JSON.parse('{}'));                 // {}  object
 console.log(parseJSON('{}'));                 // {}  object
 console.log(JSON.parse('true'));               // true  boolean
@@ -88,8 +143,8 @@ console.log(JSON.parse('[1, 5, "false"]'));    // [1, 5, "false"]  object
 console.log(parseJSON('[1, 5, "false"]'));    // [1, 5, "false"]  object
 console.log(JSON.parse('null'));               // null  object
 console.log(parseJSON('null'));               // null  string
-console.log(JSON.parse('{"1": 1, "2": 2}'));   //Object {1: 1, 2: 2}  object
-console.log(parseJSON('{"1": 1, "2": 2}'));   //Object {1: 1, 2: 2}  string
+console.log(JSON.parse('{"1": 1, "2": 2, "a":  "a"}'));   //Object {1: 1, 2: 2}  object
+console.log(parseJSON('{"1": 1, "2": 2, "b":  "a"}'));   //Object {1: 1, 2: 2}  string
 
 
 
